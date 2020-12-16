d8cecf68a08875d36ec3dee7644b12ae87b562e6
noidfrx <58480414+noidfrx@users.noreply.github.com>
2020-12-16 20:50:19 -0300

    Se finaliza primera pantalla con sus comprobaciones

--- app/src/main/java/com/noidfrx/aprendiendo_android_s4_form/MainActivity.java
+++ app/src/main/java/com/noidfrx/aprendiendo_android_s4_form/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnMainSiguiente;
+    private EditText etNombrePersona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_myicon);
         btnMainSiguiente=findViewById(R.id.btnMainSiguiente);
+        etNombrePersona = findViewById(R.id.etNombrePersona);
 
         //lo que pasa al tocar boton
         btnMainSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(i);
+
+                String nombre = etNombrePersona.getText().toString();
+                if(!nombre.isEmpty() && nombre != null){
+                    i.putExtra("nombre", nombre);
+                    startActivity(i);
+                }else{
+                    etNombrePersona.setError("Ingrese nombre");
+                }
+
             }
         });
     }
--- app/src/main/java/com/noidfrx/aprendiendo_android_s4_form/SecondActivity.java
+++ app/src/main/java/com/noidfrx/aprendiendo_android_s4_form/SecondActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
     private Button btnSecondSiguiente;
-
+    private String nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,11 @@ public class SecondActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         btnSecondSiguiente = findViewById(R.id.btnSecondSiguiente);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null){
+            nombre = bundle.getString("nombre");
+            Toast.makeText(SecondActivity.this, "Su nombre es "+nombre, Toast.LENGTH_SHORT).show();
+        }
 
         btnSecondSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
